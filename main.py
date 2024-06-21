c633fa0357d81cc5f1904f4149d9baab0beabf5b
Burtov4ik <118672866+Burtov4ik@users.noreply.github.com>
2024-06-21 13:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,10 +90,10 @@ def get_auths_command(update: Update, context):
     ssh_command = "last -n 10"
     return execute_ssh_command(update, context, ssh_command)
 
-def get_critical_command(update, context):
+def get_critical(update, context):
     """Отправить последние 5 критических событий"""
     ssh_command = "journalctl -p crit | tail -n 5"
-    result = run_ssh_command(update, context, ssh_command)
+    result = execute_ssh_command(ssh_command)
 
     if "No entries" in result or "-- No entries --" in result:
         result = (
